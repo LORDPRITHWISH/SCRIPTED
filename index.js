ed0dd4ce0af8dcd068cea9b802b444cce4e9cbce
Prithwish Chakraborty <prithwishchakaborty05@gmail.com>
2024-01-19 01:22:25 +0530

    started

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('hi')
+
+//this is a allert
+// window.alert('hello friend')
+// window.alert('useless')
+console.log('amigo')
+let t=108;
+let tot=1199;
+let nam ="PRITHWISH"
+let b=true
+document.getElementById('hpa').textContent = `num is ${t} fog ${b}`;
+document.getElementById('nu').textContent = tot;
+console.log(typeof t)
+t**=2;
+tot++;
+console.log(`num is ${tot} fog`)
+console.log(`this ${t} is of type ${typeof t}`)
+console.log(`this ${tot} is of type ${typeof tot}`)
+console.log(`this ${b} is of type ${typeof b}`)
+console.log(`this ${nam} is of type ${typeof nam}`)
+
